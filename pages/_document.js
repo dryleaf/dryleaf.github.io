6a356ecf8575295427e23190281f6b6a9a9bdb6e
Josimar Lopes <jossylopes@hotmail.com>
2022-10-22 19:56:41 +0900

    Removes unused noflash

--- pages/_document.js
+++ pages/_document.js
@@ -24,7 +24,6 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
-          <script src="noflash.js" />
           <Main />
           <NextScript />
         </body>
