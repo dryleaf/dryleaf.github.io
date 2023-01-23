19045a4fddbc2e9b86eec5d0cd820170fba23e06
Josimar Lopes <jossylopes@hotmail.com>
2023-01-23 10:55:07 +0900

    Updates highlights

--- data/highlights.js
+++ data/highlights.js
@@ -1,15 +1,15 @@
 export const highlights = [
   {
     key: "System Design",
-    value: "Lead a small team to plan, design and implement a content aggregation website service. The aggregated contents include different contents of interests such a recommendation system, news articles and other relevant content."
+    value: "Lead a small team to plan, design and implement a content aggregation web service. The website included different services such a recommendation system, news articles, weather and other relevant information to users."
   },
   {
     key: "CI/CD",
-    value: "Improved code validation checks, unit, integration and regression tests, by adding Jenkins to the team's development stack, which ended up reducing developer's time to build, test and deploy software."
+    value: "Improved code validation checks, unit, integration and regression tests, by adding Jenkins to the team's development stack, which helped reduce the developer's time to build, test and deploy software."
   },
   {
     key: "PWA",
-    value: "Refactored an online learning platform website to improve speed, performance, and user experience by adhering to PWA best practice's design principles."
+    value: "Refactored an online learning platform website, to improve speed, performance, and user experience by adhering to Progressive Web Apps (PWA) best practice's design principles."
   },
   {
     key: "Migration Design",
@@ -21,6 +21,6 @@ export const highlights = [
   },
   {
     key: "AMP",
-    value: "Proposed to the team and implemented AMP page versions of all web services, when the AMP project was introduced."
+    value: "Proposed to the team and implemented AMP page versions of all web services, when the AMP project was being adopted."
   }
 ]
--- data/projects.js
+++ data/projects.js
@@ -2,16 +2,16 @@ export const projects = [
   {
     name: "Dictionary Service",
     description: "Plan, design, and develop an online dictionary website. The architecture design allows our application to survive under different scenarios such as high traffic, network connectivity issues. The frontend App uses PWA design principles and provides an offline experience just like a native App.",
-    tags: ["Java", "JavaScript", "Spring Boot", "NextJs", "Redis", "Elastic Search", "Mysql", "Nginx", "Keycloak", "docker", "Kubernetes", "GCP"]
+    tags: ["Java", "JavaScript", "Spring Boot", "NextJs", "Redis", "Elastic Search", "Mysql", "Nginx", "Keycloak", "Docker", "Kubernetes", "GCP"]
   },
   {
     name: "Automated Deployment of Mobile Network Nodes",
     description: "In Mobile communication networks, nodes or sites contain antennas, which allow for Mobile communication. In this project, we created tools that generate scripts automatically to be loaded on the nodes for initial installation of the site.",
-    tags: ["C#", "Python", "PostgreSql", "API", "docker"]
+    tags: ["C#", "Python", "PostgreSql", "API", "Docker", "Ericsson RAN"]
   },
   {
     name: "https://dryleaf.io",
     description: "Personal portfolio website, hobbies and blog. Deployed on github pages. Uses github actions for CI/CD.",
-    tags: ["NextJs", "JavaScript", "docker"]
+    tags: ["NextJs", "JavaScript", "Docker", "AWS"]
   }
 ]
